726b199854635439f6f0ec74ed7c4f752041154f
Smithiboss <jamesbond.15091995@gmail.com>
2025-08-09 16:53:08 +0200

    Added parse

--- src/main/java/org/example/ecc/Signature.java
+++ src/main/java/org/example/ecc/Signature.java
@@ -2,6 +2,8 @@ package org.example.ecc;
 
 import org.example.Utils.Bytes;
 
+import java.io.ByteArrayInputStream;
+
 public record Signature(Int r, Int s) {
 
     /**
@@ -13,24 +15,48 @@ public record Signature(Int r, Int s) {
         byte[] rBin = toUnsignedDer(r);
         byte[] sBin = toUnsignedDer(s);
 
-        byte[] sequence = new byte[6 + rBin.length + sBin.length];
-        int pos = 0;
+        return Bytes.concat(new byte[]{0x30}, new byte[]{(byte) (rBin.length + sBin.length)}, rBin, sBin);
+    }
 
-        byte[] der = Bytes.concat(new byte[]{0x03},
-                Bytes.concat(new byte[]{(byte) (rBin.length + sBin.length)}, Bytes.concat(rBin, sBin)));
-
-        sequence[pos++] = 0x30;
-        sequence[pos++] = (byte) (rBin.length + sBin.length + 4);
-        sequence[pos++] = 0x02;
-        sequence[pos++] = (byte) rBin.length;
-        System.arraycopy(rBin, 0, sequence, pos, rBin.length);
-        pos += rBin.length;
-        sequence[pos++] = 0x02;
-        sequence[pos++] = (byte) sBin.length;
-        System.arraycopy(sBin, 0, sequence, pos, sBin.length);
-
-        return der;
-
+    /**
+     * Parse
+     *
+     * @param sigBin a {@code byte} array
+     * @return a {@link Signature} object
+     */
+    public Signature parse(byte[] sigBin) {
+        var sigStream = new ByteArrayInputStream(sigBin);
+        var compound = Hex.parse(sigStream.read());
+        // check if first byte is 0x30
+        if (compound.ne(Hex.parse("30"))) {
+            throw new IllegalArgumentException("Bad signature");
+        }
+        var length = Hex.parse(sigStream.read());
+        // check if overall length matches
+        if (length.add(Int.parse(2)).ne(Int.parse(sigBin.length))) {
+            throw new IllegalArgumentException("Bad signature length");
+        }
+        var marker = Hex.parse(sigStream.read());
+        // check if marker is 0x02
+        if (marker.ne(Hex.parse("02"))) {
+            throw new IllegalArgumentException("Bad signature");
+        }
+        var rLength = Hex.parse(sigStream.read());
+        // read r
+        var r = Hex.parse(Bytes.read(sigStream, rLength.intValue()));
+        marker = Hex.parse(sigStream.read());
+        // check if marker is 0x02
+        if (marker.ne(Hex.parse("02"))) {
+            throw new IllegalArgumentException("Bad signature");
+        }
+        var sLength = Hex.parse(sigStream.read());
+        // read s
+        var s = Hex.parse(Bytes.read(sigStream, sLength.intValue()));
+        // check if length of input byte array equals length of r + s + 6
+        if (sigBin.length != rLength.intValue() + sLength.intValue() + 6) {
+            throw new IllegalArgumentException("Signature too long");
+        }
+        return new Signature(r, s);
     }
 
     /**
@@ -42,12 +68,11 @@ public record Signature(Int r, Int s) {
     private byte[] toUnsignedDer(Int value) {
         // Format to 32 bytes and remove leading 0x00 bytes
         byte[] raw = Bytes.lStrip(value.toBytes(32));
-
+        // Add marker 0x00 if raw is signed
         if ((raw[0] & 0x80) != 0) {
-            // Add marker 0x00 if raw is signed
             raw = Bytes.concat(new byte[]{0x00}, raw);
         }
-        // Add marker 0x02
+        // Add marker byte 0x02 and length of raw
         var markers = new byte[]{0x02, (byte) raw.length};
         raw = Bytes.concat(markers, raw);
 

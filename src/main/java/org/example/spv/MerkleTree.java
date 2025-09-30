ddaa1e0b583013847c1f59ed644a3a3167cb2989
Smithiboss <jamesbond.15091995@gmail.com>
2025-09-30 11:55:07 +0200

    Added doc

--- src/main/java/org/example/spv/MerkleTree.java
+++ src/main/java/org/example/spv/MerkleTree.java
@@ -14,12 +14,16 @@ public class MerkleTree {
 
     public MerkleTree(int total) {
         this.total = total;
+        // calculate max depth
         this.maxDepth = (int) Math.ceil(Helper.log(total, 2));
         this.nodes = new ArrayList<>();
-
+        // initialize the tree with null nodes for every node on every tree level
         for (int depth = 0; depth < maxDepth + 1; depth++) {
+            // calculate the number of nodes at this depth
             var numItems = (int) Math.ceil(this.total / Math.pow(2, this.maxDepth - depth));
+            // initialize the list of nodes at this depth with null nodes
             List<byte[]> levelHashes = new ArrayList<>(Collections.nCopies(numItems, null));
+            // add the list of nodes at this depth to the list of all nodes at all depths
             nodes.add(levelHashes);
         }
         
@@ -27,45 +31,82 @@ public class MerkleTree {
         currentIndex = 0;
     }
 
+    /**
+     * Move up a tree level
+     */
     public void up() {
         currentDepth -= 1;
         currentIndex /= 2;
     }
 
+    /**
+     * Move to the left child of the current node
+     */
     public void left() {
         currentDepth += 1;
         currentIndex *= 2;
     }
 
+    /**
+     * Move to the right child of the current node
+     */
     public void right() {
         currentDepth += 1;
         currentIndex = currentIndex * 2 + 1;
     }
 
+    /**
+     * Returns the root of the tree
+     * @return a {@code byte} array
+     */
     public byte[] root() {
         return nodes.getFirst().getFirst();
     }
 
+    /**
+     * Sets the current node to the given value
+     * @param value a {@code byte} array
+     */
     public void setCurrentNode(byte[] value) {
         nodes.get(currentDepth).set(currentIndex, value);
     }
 
+    /**
+     * Returns the current node
+     * @return a {@code byte} array
+     */
     public byte[] getCurrentNode() {
         return nodes.get(currentDepth).get(currentIndex);
     }
 
+    /**
+     * Returns the left child of the current node
+     * @return a {@code byte} array
+     */
     public byte[] getLeftNode() {
         return nodes.get(currentDepth + 1).get(currentIndex * 2);
     }
 
+    /**
+     * Returns the right child of the current node
+     * @return a {@code byte} array
+     */
     public byte[] getRightNode() {
         return nodes.get(currentDepth + 1).get(currentIndex * 2 + 1);
     }
 
+    /**
+     * Returns whether the current node is a leaf node
+     * @return a {@code boolean}
+     */
     public boolean isLeaf() {
         return currentDepth == maxDepth;
     }
 
+    /**
+     * Returns whether the right child of the current node exists
+     * @return a {@code boolean}
+     */
     public boolean rightExists() {
         return nodes.get(currentDepth + 1).size() > currentIndex * 2 + 1;
     }
@@ -77,8 +118,8 @@ public class MerkleTree {
      */
     public void populateTree(byte[] flagBitsArray, List<byte[]> hashesList) {
         Deque<Byte> flagBits = new ArrayDeque<>();
-        for (byte b : flagBitsArray) {
-            flagBits.push(b);
+        for (int i = flagBitsArray.length - 1; i >= 0; i--) {
+            flagBits.push(flagBitsArray[i]);
         }
         Deque<byte[]> hashes = new ArrayDeque<>(hashesList);
 
@@ -111,15 +152,12 @@ public class MerkleTree {
             }
         }
         if (!hashes.isEmpty()) {
-            throw new IllegalStateException("Not all hashes were consumed");
+            throw new RuntimeException("Not all hashes were consumed");
         }
         for (Byte flagBit : flagBits) {
             if (flagBit != 0) {
-                throw new IllegalStateException("Not all flag bits were consumed");
+                throw new RuntimeException("Not all flag bits were consumed");
             }
         }
     }
-
-
-
 }
